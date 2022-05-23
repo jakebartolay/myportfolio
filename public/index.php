f5357427bfc54aeb9f48a282e116da87a06e08d0
jake bartolay <lilkraceboy@gmail.com>
2022-05-24 02:08:02 +0800

    init

---
+++ public/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bartolay Jake</title>
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://zukameku.github.io/jakebartolay/public/css.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+</head>
+
+<body>
+
+  <!--HEADER-->
+  <div class="row-cols-auto text-center">
+    <img src="https://zukameku.github.io/jakebartolay/public/profile/ben19.jpg" class="img-dp" alt="jakebartolay"/>
+  </div>
+  <div class="container-fluid">
+    <header class="text-center p-1">
+      <h6 class="text-md">
+        <spann content="J">J</spann>
+          <spann content="a">a</spann>
+          <spann content="k">k</spann>
+          <spann content="e">e</spann>
+          <spann content="B">B</spann>
+          <spann content="a">a</spann>
+          <spann content="r">r</spann>
+          <spann content="t">t</spann>
+          <spann content="o">o</spann>
+          <spann content="l">l</spann>
+          <spann content="a">a</spann>
+          <spann content="y">y</spann>
+          <div class="spinner-border sub-color" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+      </h4>
+      <p class="sub-color">PROGRAMMER</p>
+    </header>
+  </div>
+  <!--BODY-->
+  <div class="container-fluid">
+      <div class="container p-3">
+        <div class="shadow-lg p-3 mb-4 rounded text-md">
+          <!--SKILLS-->
+          <div class="row justify-content">
+            <div class="col-4">
+              <h3 class="sub-color p-2">
+                #<span class="text-white">SKILLS</span>
+              </h3>
+            </div>
+            <hr class="new4" />
+          </div>
+          <div class="row justify-content">
+            <div class="col-lg-10">
+              <h6>
+              <span class="p-0">// PROGRAMMING LANGUAGE //</span>
+            </h6>
+            </div>
+          </div>
+          <div class="row justify-content">
+            <div class="col-4">
+              <h4 class="sub-color p-2">
+                >_ <span class="text-white">Java&nbsp; : "6/10"</span>
+              </h4>
+            </div>
+          </div>
+          <div class="row justify-content">
+            <div class="col-4">
+              <h4 class="sub-color p-2">
+                >_ <span class="text-white">C#&nbsp;&nbsp;&nbsp; : "4/10"</span>
+              </h4>
+            </div>
+          </div>
+          <div class="row justify-content">
+            <div class="col-4">
+              <h4 class="sub-color p-2">
+                >_ <span class="text-white">C++&nbsp;&nbsp; : "5/10"</span>
+              </h4>
+            </div>
+          </div>
+
+
+          <!--EXPERIENCES-->
+
+          <div class="row justify-content">
+            <hr class="new4" />
+            <div class="col-4">
+              <h3 class="sub-color p-2">
+                #<span class="text-white">EXPERIENCES</span>
+              </h3>
+            </div>
+          </div>
+          <div class="row justify-content-between">
+            <div class="col-4">
+              <h6 class="sub-color p-2">
+                > <span class="text-white">NONE</span> _
+              </h6>
+            </div>
+            <div class="col-4">
+              <h6 class="sub-color p-2">
+                > <span class="text-white">NONE</span> _
+              </h6>
+            </div>
+          </div>
+          <div class="row justify-content-between">
+            <div class="col-4">
+              <h6>
+                // <little class="text-white">
+                  </little>
+              </h6>
+            </div>
+            <div class="col-4">
+              <h6>
+                // <little class="text-white">
+                  </little>
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="footer">RESUME - Jake Bartolay</p>
+      </div>
+    </div>
+</body>
+
+</html>
